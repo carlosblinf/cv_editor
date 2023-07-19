7d3b7c0c9e325bdb7a77909571dfde5b8814c252
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2023-07-19 13:14:00 -0400

    update hbsCode

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,6 +2,7 @@
 //   "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
 //   "{{kids.length}} kids:</p>" +
 //   "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+// <ul class="text-left">{{#contact.items}}<li>{{type}}: {{text}}</li>{{/contact.items}}</ul>
 export const hbsCode = `
     <div class="max-w-[50%] h-full shadow-2xl py-5 flex flex-row">
       <aside class="flex flex-col items-center w-screen gap-1 py-6 bg-green-700">
@@ -17,13 +18,18 @@ export const hbsCode = `
       {{#with Education as | education |}}
         <section class="text-center mt-20">
           <h1 class="text-lg">{{education.header}}</h1>
-          <ul>{{#education.items}}<li>{{degree}} <span class='block text-xs'>{{date}}</span></li>{{/education.items}}</ul>
+          <div>{{education.degree}}</div>
+          <div class='block text-xs'>{{education.date}}</div>
         </section>
       {{/with}}
       {{#with Contact as | contact |}}
         <section class="text-center mt-20">
           <h1 class="text-lg">{{contact.header}}</h1>
-          <ul class="text-left">{{#contact.items}}<li>{{type}}: {{text}}</li>{{/contact.items}}</ul>
+          <ul class="text-left">
+          <li>email: {{contact.email}}</li>
+          <li>phone: {{contact.phone}}</li>
+          <li>cite: {{contact.cite}}</li>
+          </ul>
         </section>
       {{/with}}
       </aside>
@@ -39,7 +45,7 @@ export const hbsCode = `
         <section class='mt-8'>
           <h1 class='text-2xl'>{{skills.header}}</h1>
           <hr class="my-2 h-0.5 border-t-0 bg-neutral-500 opacity-100 dark:opacity-50" />
-          <ul>{{#skills.items}}<li>{{text}}</li>{{/skills.items}}</ul>
+          <p>{{skills.text}}</p>
         </section>
       {{/with}}
       </main>
@@ -60,7 +66,7 @@ export const dataInfo = {
     type: "Profile",
     profession: "Senior Developer",
     profileImageURL:
-     'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(4).webp',
+      "https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(4).webp",
     display: true,
     about:
       "Recent college graduate with experience in various areas of software engineering, including infrastructure and data analytics. A fast learner who thrives on generating innovative ideas, trouble-shooting and problem-solving, and working with object-oriented programming languages including Python and Java. ",
@@ -69,6 +75,8 @@ export const dataInfo = {
   Education: {
     header: "Education",
     type: "Education",
+    degree: "Software Engineering - University of Sydney",
+    date: "Mar 2017 - Dec 2019",
     display: true,
     items: [
       {
@@ -189,6 +197,9 @@ export const dataInfo = {
     header: "Contact",
     type: "Contact",
     display: true,
+    phone: "+61 01 012121",
+    email: "eliot@example.com",
+    cite: "www.eliotdev.com",
     items: [
       {
         type: "phone",
