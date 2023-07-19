5b56b45844d13026c35c1e7099e8abc736af95d9
Kevin Coto <kevincoto2002@gmail.com>
2023-07-19 10:10:20 -0400

    fix: updateData

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -7,6 +7,16 @@ import { useEffect, useLayoutEffect, useState } from "react";
 import useDebounce from "../../utils/debounce";
 import { dataInfo, hbsCode } from "@/utils/data";
 import parse from "html-react-parser";
+import { Input } from "@material-tailwind/react";
+interface Profile {
+  name: string,
+  type: string,
+  profession: string,
+  profileImageURL: string,
+  display: boolean,
+  about: string,
+}
+
 
 function Viewer() {
   // const info = store.getState().builder;
@@ -26,23 +36,43 @@ function Viewer() {
     setJsx(dataSaved);
   }
 
-  const handleOnChange = (
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLSelectElement>
-  ): void => {
-    setData({ ...data, [e.target.name]: e.target.value });
+  const updateData = (
+    event: 
+    HTMLInputElement
+    
+  ) => {
+    const name: keyof Profile  = event.name as string
+    const ariaRoleDescription = event.ariaRoleDescription
+    const value = event.value
+    setData((prevData) => {
+      const newData = {...prevData}
+      // if(ariaRoleDescription == 'Profile'){
+      // newData = value
+      // return newData
+      console.log('name:', name)
+      console.log('ariaRole:', ariaRoleDescription)
+      console.log('value', value)
+      
+      newData.Profile[name] = value
+     
+     return newData
+    }
+    )
   };
 
   return (
-    <div>
-      <h1>Viewer</h1>
-      <input
-        placeholder="Nombre"
+    <div className="w-screen h-screen flex bg-gray-300">
+      <div className="w-1/2">
+      <Input
+        variant="standard" 
         name="name"
-        onChange={handleOnChange}
-        defaultValue={data.name}
+
+        label="Nombre"
+        aria-roledescription="Profile"
+        onChange={(e) => updateData(e.target)}
+        defaultValue={data.Profile.name}
       />
+      </div>
 
       {jsx && parse(jsx)}
     </div>
